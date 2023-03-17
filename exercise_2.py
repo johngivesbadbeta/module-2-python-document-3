45e9fcfac6efb10107bd64dc7466ffa699499945
John Pinano <jpinano@gmail.com>
2023-03-17 08:51:27 -0700

    first commit

---
+++ exercise_2.py
@@ -0,0 +1,9 @@
+def measurements(length, width):
+    square_footage = str(length * width)
+    return "Your house is " + square_footage + " square feet."
+
+import math
+
+def circle_circ(radius):
+    circumference = 2 * math.pi * radius
+    return "The circle's circumference is " + str(circumference) + "."
